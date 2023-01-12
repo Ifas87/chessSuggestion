1f8ad711a588474e6cbf84540565f91f9c23696d
Safi <safi.sharieff@gmail.com>
2023-01-12 20:09:21 +0400

    Added interface to communicate with the stockfish bot

--- stockFishInterface.py
+++ stockFishInterface.py
@@ -1,7 +1,10 @@
 import os
+from stockfish import Stockfish
 
 def main():
-    pass
+    dafish = Stockfish(path="stocksifh/stockfish-windows-2022-x86-64-modern")
+    dafish.set_position(["e2e4", "e7e6"])
+    print(dafish.get_best_move())
 
 if __name__=='__main__':
     main()
